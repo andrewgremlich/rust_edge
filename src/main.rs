07ae0821e6d8e85017796f65076528b4c6bc9a55
Andrew Jacob Gremlich <andrew.gremlich@ldschurch.org>
2019-02-15 14:17:09 -0700

    Able to pull game config file and print.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serde_json::Value;
+use std::env;
+
+//Maybe use TUI_RS?  https://github.com/fdehau/tui-rs
+// Another one... https://github.com/gyscos/Cursive
+// and another one...https://github.com/redox-os/termion
+//  They are all pretty even.
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let difficulty = &args[1];
+
+    let game_config_string = include_str!("config.json");
+    let game_config: Value = serde_json::from_str(game_config_string).unwrap();
+
+    println!("{}", game_config[difficulty]);
 }
